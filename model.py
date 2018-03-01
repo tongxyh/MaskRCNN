bef33181ce2323062aca2c0a9dfad527caa71b95
Tong Chen <tongxyh@163.com>
2018-03-01 17:16:14 +0800

    add inspect_checkpoint

--- model.py
+++ model.py
@@ -6,3 +6,10 @@ def resnet_v50():
 def raw_feature_extractor():
     #ZF or VGG16
     pass
+#import the inspect_checkpoint library
+from tensorflow.python.tools import inspect_checkpoint as chkp
+
+chkp.print_tensors_in_checkpoint_file("data/resnet_v1_50.ckpt",tensor_name='',all_tensors=False) #set False to only print tensor name and shape
+
+saver = tf.train.Saver()
+saver.restore()
