f3d20984c9463314e228e5f11751aede1e972a52
Tong Chen <tongxyh@163.com>
2018-03-30 19:15:53 +0800

    modified cls_score_layer

--- debug.py
+++ debug.py
@@ -1,3 +1,6 @@
+#import os
+#os.system("export CUDA_VISIBLE_DEVICES=1")
+
 import tensorflow as tf
 from modules import anchor
 from utili.datasets import dataset_factory as datasets
@@ -15,18 +18,22 @@ image, ih, iw, gt_boxes, gt_masks, num_instances, img_id, iterator = \
                              FLAGS.dataset_dir,
                              FLAGS.im_batch,
                              is_training=False)
-print(image)
 
 stride = 8
+
 
 with tf.Session() as sess:
     sess.run(iterator.initializer)
     for i in range(100):
         gt_bboxes,height,width = sess.run([gt_boxes,ih,iw])
 
-        all_anchors = anchor.anchor_plane(width/stride,height/stride,stride = 8)
-        anchor.anchor_sample(all_anchors,gt_bboxes)
+all_anchors = anchor.anchor_plane(width/stride,height/stride,stride = 8)
+anchor_s = anchor.anchor_sample(all_anchors,gt_boxes)
 
+# TODO: bbox regression loss
+#anchor_s
+
+# TODO: score loss
 
 #plt.imshow(abs(image[0]))
 #plt.show()
